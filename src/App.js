d72e054add25c3092c404b95b216b7bd7e2dbc3e
HoangPhuc2707 <tranp8639@gmail.com>
2024-08-23 23:11:37 +0700

    handle-State-Sign-in-Sign-up

--- src/App.js
+++ src/App.js
@@ -1,9 +1,8 @@
-import React, { Fragment, useEffect } from 'react'
+import React, { Fragment } from 'react'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { routes } from './routes';
 import DefaultComponent from './components/DefaultComponent/DefaultComponent';
-import axios from 'axios';
-import { useQuery } from '@tanstack/react-query';
+
 
 
 function App() {
@@ -11,13 +10,13 @@ function App() {
   // useEffect(() => {
   //   fetchApi()
   // }, [])
-  const fetchApi = async () => {
-    const res = await axios.get(`${process.env.REACT_APP_API_URL}/product/get-all`)
-    return res.data
-  }
+  // const fetchApi = async () => {
+  //   const res = await axios.get(`${process.env.REACT_APP_API_URL}/product/get-all`)
+  //   return res.data
+  // }
 
-  const query = useQuery({ query: ['todos'], queryFn: fetchApi })
-  console.log('query',query)
+  // const query = useQuery({ query: ['todos'], queryFn: fetchApi })
+  // console.log('query',query)
 
   return (
 
