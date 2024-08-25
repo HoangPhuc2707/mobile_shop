4c8d5c744b9c4a43f26f8134eabc25de28d5b8fd
HoangPhuc2707 <tranp8639@gmail.com>
2024-08-25 22:28:45 +0700

    handle-info-user-after-login

--- src/components/HeaderComponent/HeaderComponent.jsx
+++ src/components/HeaderComponent/HeaderComponent.jsx
@@ -4,15 +4,19 @@ import { WrapperHeader, WrapperTextHeader, WrapperHeaderAccout, WrapperTextHeade
 import { UserOutlined, CaretDownOutlined, ShoppingCartOutlined } from '@ant-design/icons';
 import ButtonInputSearch from '../ButtonInputSearch/ButtonInputSearch';
 import { useNavigate } from "react-router-dom";
+import { useSelector } from "react-redux";
 
 
 const HeaderComponent = () => {
     const navigate = useNavigate()
+    const user = useSelector((state) => state.user)
+
     const handleNavigateLogin = () => {
         navigate('sign-in')
     }
+    console.log('user', user)
     return (
-        <div style={{ width: '100%', background: 'rgb(26, 148, 255)', display: 'flex', justifyContent: 'center'}}>
+        <div style={{ width: '100%', background: 'rgb(26, 148, 255)', display: 'flex', justifyContent: 'center' }}>
             <WrapperHeader>
                 <Col span={5}>
                     <WrapperTextHeader>TiKi</WrapperTextHeader>
@@ -27,15 +31,20 @@ const HeaderComponent = () => {
                 <Col span={6} style={{ display: 'flex', gap: '20px', alignItems: 'center' }}>
                     <WrapperHeaderAccout>
                         <UserOutlined style={{ fontSize: '30px' }} />
-                        <div onClick={handleNavigateLogin} style={{ cursor: 'pointer'}}>
-                            <WrapperTextHeaderSmall>Đăng nhập/Đăng ký</WrapperTextHeaderSmall>
-                            <div>
-                                <WrapperTextHeaderSmall>Tài khoản</WrapperTextHeaderSmall>
-                                <CaretDownOutlined />
+                        {user?.name ? (
+                            <div style={{ cursor: 'pointer' }}>{user.name}</div>
+                        ) : (
+                            <div onClick={handleNavigateLogin} style={{ cursor: 'pointer' }}>
+                                <WrapperTextHeaderSmall>Đăng nhập/Đăng ký</WrapperTextHeaderSmall>
+                                <div>
+                                    <WrapperTextHeaderSmall>Tài khoản</WrapperTextHeaderSmall>
+                                    <CaretDownOutlined />
+                                </div>
                             </div>
-                        </div>
+                        )}
+
                     </WrapperHeaderAccout>
-                    <div style={{ display: 'flex'}}>
+                    <div style={{ display: 'flex' }}>
                         <Badge count={4} size="small">
                             <ShoppingCartOutlined style={{ fontSize: '30px', color: '#fff' }} />
                         </Badge>
