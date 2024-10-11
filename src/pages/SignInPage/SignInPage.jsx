1ad238175ffef3c7dcbe12473cf62f680ced635f
HoangPhuc2707 <tranp8639@gmail.com>
2024-10-11 15:42:37 +0700

    add order product

--- src/pages/SignInPage/SignInPage.jsx
+++ src/pages/SignInPage/SignInPage.jsx
@@ -5,7 +5,7 @@ import ButtonComponent from "../../components/ButtonComponent/ButtonComponent";
 import imageLogin from "../../assets/images/logo-login.png";
 import { Image } from "antd";
 import { EyeFilled, EyeInvisibleFilled } from '@ant-design/icons';
-import { useNavigate } from "react-router-dom";
+import { useLocation, useNavigate } from "react-router-dom";
 import * as UserService from '../../services/UserService';
 import { useMutationHooks } from "../../hooks/useMutationHook";
 import Loading from "../../components/LoadingComponent/Loading";
@@ -16,6 +16,7 @@ import { updateUser } from "../../redux/slides/userSlide";
 const SignInPage = () => {
     const navigate = useNavigate()
     const [isShowPassword, setIsShowPassword] = useState(false)
+    const location = useLocation()
     const [email, setEmail] = useState('')
     const [password, setPassword] = useState('')
     const dispatch = useDispatch();
@@ -24,14 +25,18 @@ const SignInPage = () => {
         data => UserService.loginUser(data)
     )
     const { data, isPending, isSuccess } = mutation
-    
+
     useEffect(() => {
         if (isSuccess) {
-            navigate('/')
+            if (location?.state) {
+                navigate(location?.state)
+            } else {
+                navigate('/')
+            }
             localStorage.setItem('access_token', JSON.stringify(data?.access_token))
             if (data?.access_token) {
                 const decoded = jwtDecode(data?.access_token)
-                if (decoded?.id){
+                if (decoded?.id) {
                     handleGetDetailsUser(decoded?.id, data?.access_token)
                 }
             }
@@ -40,7 +45,7 @@ const SignInPage = () => {
 
     const handleGetDetailsUser = async (id, token) => {
         const res = await UserService.getDetailsUser(id, token)
-        dispatch(updateUser({...res?.data, access_token: token}))
+        dispatch(updateUser({ ...res?.data, access_token: token }))
     }
 
     console.log('mutation', mutation)
